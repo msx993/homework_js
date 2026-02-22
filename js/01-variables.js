d960824c6408e0b40af94bc836dedc2bb9cb1f16
msx993 <mivanovych2012@gmail.com>
2026-02-22 12:25:21 +0200

    дз2

--- js/01-variables.js
+++ js/01-variables.js
@@ -1,31 +1,31 @@
-const myName = "max";
-console.log(myName)
+// const myName = "max";
+// console.log(myName)
 
-console.log(typeof myName);
+// console.log(typeof myName);
 
-const myAge = 13;
-console.log(myAge)
+// const myAge = 13;
+// console.log(myAge)
 
-console.log(typeof myAge);
+// console.log(typeof myAge);
 
-const isStudent = true;
+// const isStudent = true;
 
-console.log(typeof isStudent);
+// console.log(typeof isStudent);
 
-const myString = "п'яний чоловік не падає він атакує підлогу";
-console.log(myString)
+// const myString = "п'яний чоловік не падає він атакує підлогу";
+// console.log(myString)
 
-const myNumber = 25;
-console.log(typeof myNumber);
+// const myNumber = 25;
+// console.log(typeof myNumber);
 
-const isComing = confirm("Ви дійсно хочете покинути сторінку?");
-console.log(typeof isComing);
+// const isComing = confirm("Ви дійсно хочете покинути сторінку?");
+// console.log(typeof isComing);
 
-const userName = prompt("Введіть своє ім’я");
-console.log(typeof userName);
+// const userName = prompt("Введіть своє ім’я");
+// console.log(typeof userName);
 
-const isAlert = alert("Увага! Дія може бути небезпечною.");
-console.log(typeof isComing);
+// const isAlert = alert("Увага! Дія може бути небезпечною.");
+// console.log(typeof isComing);
 
-const isComfirm = confirm("Ви впевнені, що хочете продовжити?")
-console.log(typeof isComing);
+// const isComfirm = confirm("Ви впевнені, що хочете продовжити?")
+// console.log(typeof isComing);
---
+++ js/02-hw.js
@@ -0,0 +1,36 @@
+// const celsius = 25
+// const fahrenheit = 77
+// console.log (celsius * 1,8 + 32);
+
+
+const daysInMonth = 30;
+const hoursInMonth = Math.floor(daysInMonth * 24)
+const minutesInMonth = Math.floor(hoursInMonth * 60)
+console.log(daysInMonth, hoursInMonth, minutesInMonth);
+
+
+const health = 100;
+const energy = 100;
+const energyAfter = Math.floor(energy - 30);
+const healthAfter = Math.floor(health - 20);
+console.log(health, energy);
+
+
+const totalPrice = 100;
+const discountedPrice = Math.floor(totalPrice - 10);
+console.log(discountedPrice);
+
+const floatString = "45.67"
+console.log(parseFloat(floatString));
+
+const intString = "123"
+console.log(parseInt(intString));
+
+const number = 64;
+console.log(Math.sqrt(number));
+
+
+const integer = 42 
+const stringNumber = "256"
+console.log(parseInt(stringNumber));
+console.log(toString(integer));
