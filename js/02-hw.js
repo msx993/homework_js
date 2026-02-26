d991dfcf57a4075105866d7032ff6838309811d3
msx993 <mivanovych2012@gmail.com>
2026-02-26 15:39:24 +0200

    дз 3

--- js/02-hw.js
+++ js/02-hw.js
@@ -3,34 +3,34 @@
 // console.log (celsius * 1,8 + 32);
 
 
-const daysInMonth = 30;
-const hoursInMonth = Math.floor(daysInMonth * 24)
-const minutesInMonth = Math.floor(hoursInMonth * 60)
-console.log(daysInMonth, hoursInMonth, minutesInMonth);
+// const daysInMonth = 30;
+// const hoursInMonth = Math.floor(daysInMonth * 24)
+// const minutesInMonth = Math.floor(hoursInMonth * 60)
+// console.log(daysInMonth, hoursInMonth, minutesInMonth);
 
 
-const health = 100;
-const energy = 100;
-const energyAfter = Math.floor(energy - 30);
-const healthAfter = Math.floor(health - 20);
-console.log(health, energy);
+// const health = 100;
+// const energy = 100;
+// const energyAfter = Math.floor(energy - 30);
+// const healthAfter = Math.floor(health - 20);
+// console.log(health, energy);
 
 
-const totalPrice = 100;
-const discountedPrice = Math.floor(totalPrice - 10);
-console.log(discountedPrice);
+// const totalPrice = 100;
+// const discountedPrice = Math.floor(totalPrice - 10);
+// console.log(discountedPrice);
 
-const floatString = "45.67"
-console.log(parseFloat(floatString));
+// const floatString = "45.67"
+// console.log(parseFloat(floatString));
 
-const intString = "123"
-console.log(parseInt(intString));
+// const intString = "123"
+// console.log(parseInt(intString));
 
-const number = 64;
-console.log(Math.sqrt(number));
+// const number = 64;
+// console.log(Math.sqrt(number));
 
 
-const integer = 42 
-const stringNumber = "256"
-console.log(parseInt(stringNumber));
-console.log(toString(integer));
+// const integer = 42 
+// const stringNumber = "256"
+// console.log(parseInt(stringNumber));
+// console.log(toString(integer));
---
+++ js/03-hw.js
@@ -0,0 +1,24 @@
+const result =(5 + 5 + "5");
+console.log(result);
+
+
+const email = "mivanovych2012@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+
+
+const a = "My";
+const b = "name";
+const c = "is";
+const fullName = ` ${a}  ${b} ${c} Vector `;
+console.log(fullName);
+
+
+
+
+const userName = "Олександро";
+const payment = 300;
+
+const fullAlert = `Дякуємо, ${userName} До сплати ${payment} гривень`;
+console.log(fullAlert);
