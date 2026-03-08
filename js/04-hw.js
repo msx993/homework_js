92f122dc2133220f6d4be6f439154a7e1bc87c76
msx993 <mivanovych2012@gmail.com>
2026-03-08 19:25:48 +0200

    дз 5

--- js/04-hw.js
+++ js/04-hw.js
@@ -1,59 +1,59 @@
-const a = 5
-const b = 5
-if (a + b === 10) {
-    console.log("Суммма дорівнює 10");
+// const a = 5
+// const b = 5
+// if (a + b === 10) {
+//     console.log("Суммма дорівнює 10");
     
-} else {
-    console.log("Суммма не дорівнює 10, або більше десяти");
-}
+// } else {
+//     console.log("Суммма не дорівнює 10, або більше десяти");
+// }
 
-const sentense = "JavaScript is awesome"
+// const sentense = "JavaScript is awesome"
 
-if (sentense.includes("JavaScript")) {
-    console.log("Текст містить слово JavaScript");
+// if (sentense.includes("JavaScript")) {
+//     console.log("Текст містить слово JavaScript");
     
-} else {
-    console.log(
+// } else {
+//     console.log(
 
-"Текст не містить слово JavaScript");
-}
+// "Текст не містить слово JavaScript");
+// }
 
 
-const number = 18;
-if (10 < number < 20) {
-    console.log("Число входить в діапазон від 10 до 20");
+// const number = 18;
+// if (10 < number < 20) {
+//     console.log("Число входить в діапазон від 10 до 20");
     
-} else {
-    console.log("Число не входить в діапазон від 10 до 20");
-}
+// } else {
+//     console.log("Число не входить в діапазон від 10 до 20");
+// }
 
 
-const Name = "Max";
-const Email = "mivanovych2012@gmail.com";
-const Password = "12345678";
-if (Name.length > 2) {
-    console.log("Перенаправлення на іншу сторінку");
+// const Name = "Max";
+// const Email = "mivanovych2012@gmail.com";
+// const Password = "12345678";
+// if (Name.length > 2) {
+//     console.log("Перенаправлення на іншу сторінку");
     
-} else {
-    console.log( "Помилка: неправильне заповнення");
-}
+// } else {
+//     console.log( "Помилка: неправильне заповнення");
+// }
 
 
 
-if (Email.includes("@")) {
-    console.log("Перенаправлення на іншу сторінку");
+// if (Email.includes("@")) {
+//     console.log("Перенаправлення на іншу сторінку");
     
-} else {
-    console.log( "Помилка: неправильне заповнення");
-}
+// } else {
+//     console.log( "Помилка: неправильне заповнення");
+// }
 
 
 
-if (Password.length > 5) {
-    console.log( "Перенаправлення на іншу сторінку");
+// if (Password.length > 5) {
+//     console.log( "Перенаправлення на іншу сторінку");
     
-} else {
-    console.log("Помилка: неправильне заповнення");
-}
+// } else {
+//     console.log("Помилка: неправильне заповнення");
+// }
 
 
---
+++ js/05-hw.js
@@ -0,0 +1,94 @@
+const drink =  "Чай"
+
+switch (drink) {
+     case "Чай":
+        console.log("Чай");
+        
+        break
+
+         case "Кава":
+             console.log("Кава");
+        break
+
+    case "Сік":
+         console.log("Сік");
+        break;
+
+    default: console.log("Ви не вибрали напиток.");
+    
+        break;
+}
+
+
+
+const day = "monday"
+
+switch (day) {
+    case "monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+
+    console.log("Будні дні");
+    
+        break;
+    case "субота":
+    case "неділя":
+
+    console.log("Вихідні");
+        break;
+
+    default: console.log("Ви не ввели день тижня");
+    
+        break;
+}
+
+const month = "7"
+
+switch (month) {
+    case "12":
+    case "1":
+    case "2":
+    console.log("Зима");
+        break;
+    case "3":
+    case "4":
+    case "5":
+    console.log("Весна");
+        break;
+            case "monday":
+    case "6":
+    case "7":
+    case "8":
+    console.log("Літо");
+        break;
+            case "monday":
+    case "9":
+    case "10":
+    case "11":
+    console.log("Осінь");
+        break;
+
+    default: console.log("Ви не ввели день тижня");
+    
+const color =  "Green"
+
+switch (color) {
+     case "Green":
+        console.log("go");
+        
+        break
+
+         case "yellow":
+             console.log("wait");
+        break
+
+    case "red":
+         console.log("stop");
+        break;
+
+    default: console.log("Ви не вибрали колір");
+    
+        break;
+}
