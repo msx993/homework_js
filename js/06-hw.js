d1acd300de7917530f6a955485173a428756e2a0
msx993 <mivanovych2012@gmail.com>
2026-03-23 14:18:33 +0200

    hw 6

--- js/06-hw.js
+++ js/06-hw.js
@@ -14,9 +14,31 @@
 // }
 
 
-for(let i = 0; i <= 10; i +=1) {
- Number (i * 7){
-    continue
- }
-    console.log(i);
+// for(let i = 0; i <= 10; i +=1) {
+//  Number (i * 7){
+//     continue
+//  }
+//     console.log(i);
+// }
+
+// const n = 10
+// for(let i = 0; i <= n; i +=1) {
+// if(i === 10){
+//    break
+// } else {
+//    console.log(i);
+   
+// }
+// }
+
+let number = 0
+while(number < 21) {
+    number += 1
+    if (number / 3 === 0) {
+      break
+    } else  {
+      continue
+    }
+    
 }
+    console.log(number);
