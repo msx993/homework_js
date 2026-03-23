4ab5df32df6d9ba74f21897a1becdaa3185a323a
msx993 <mivanovych2012@gmail.com>
2026-03-23 14:09:41 +0200

    /

---
+++ js/06-hw.js
@@ -0,0 +1,22 @@
+// let number = 0
+
+// while(number <= 10) {
+//     number += 1
+//     console.log('Число ${number}');
+    
+// }
+
+// for(let i = 0; i <= 20; i +=1) {
+//     if(i / 2 === 0) {
+//         continue
+//     }
+//     console.log(i);
+// }
+
+
+for(let i = 0; i <= 10; i +=1) {
+ Number (i * 7){
+    continue
+ }
+    console.log(i);
+}
