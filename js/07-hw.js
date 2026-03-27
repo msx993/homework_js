176a747fd2ec57a0ec2cb5b760db21b62fafc6ba
msx993 <mivanovych2012@gmail.com>
2026-03-27 13:27:55 +0200

    /

---
+++ js/07-hw.js
@@ -0,0 +1,12 @@
+// const a = [0, 1, 2]
+// a[1] = 10;
+// console.log(a);
+
+// const string = [pear, mango, carrot];
+// string[2] = banana;
+// console.log(string);
+
+const array = ["Mango", "hurries", "to", "the", "train"];
+for (i = 0; i > 6; i += 1) {
+    
+}
