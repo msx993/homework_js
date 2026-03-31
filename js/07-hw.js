3ef723f85714f97a5b1d8bf4664796940a63d717
msx993 <mivanovych2012@gmail.com>
2026-03-31 17:46:11 +0300

    Update 07-hw.js

--- js/07-hw.js
+++ js/07-hw.js
@@ -3,10 +3,22 @@
 // console.log(a);
 
 // const string = [pear, mango, carrot];
-// string[2] = banana;
+// string.splice(1,0, 'banana')
 // console.log(string);
 
-const array = ["Mango", "hurries", "to", "the", "train"];
-for (i = 0; i > 6; i += 1) {
+// const array = ["Mango", "hurries", "to", "the", "train"];
+// console.log(array);
+
+
+// const array = ["Mangos", "hurries", "to", "the", "trains"];
+// if (array.length > 5) {
+//     console.log(array);
+// }
+
+
+
+const array = [2, 4, 3, 5, 6, 22, 189, 90, 133, 24];
+for (i = 0; i <= 10; i += 1) {
     
 }
+
