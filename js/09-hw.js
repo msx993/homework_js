a9062bb30e6fb3d29673688a5a3a32ba1d419a2d
msx993 <mivanovych2012@gmail.com>
2026-04-06 13:38:19 +0300

    дз 9 половина

---
+++ js/09-hw.js
@@ -0,0 +1,28 @@
+// const logItems = function (fruits) {
+//     for (i = 0; i < fruits.length; i += 1) {
+//         console.log(`${i} - ${fruits[i]}`);
+        
+//     }
+// }
+// const fruits = ['Mango', 'Poly', 'Ajax']
+
+// const calculateEngravingPrice = function(message, pricePerWord) {
+// const result = message.length * pricePerWord
+// return result
+// }
+// calculateEngravingPrice('fruitsAndVegetables', 10)
+// console.log(calculateEngravingPrice('fruitsAndVegetables', 10));
+
+
+
+
+// const formatString = function(string) {
+// if (string.length > 40) {
+// string.pop(3)
+// string.push('...')
+// } else {
+//     console.log(string);
+    
+// }
+// }
+// formatString('fruits and vegetebles are good for you')
