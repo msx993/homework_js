fd8762a9d442dbc85c68e05a8902f1bf4440a5b5
msx993 <mivanovych2012@gmail.com>
2026-04-11 14:09:46 +0300

    z

--- js/09-hw.js
+++ js/09-hw.js
@@ -18,11 +18,20 @@
 
 // const formatString = function(string) {
 // if (string.length > 40) {
-// string.pop(3)
-// string.push('...')
+// string.slice('...')
 // } else {
 //     console.log(string);
     
 // }
 // }
 // formatString('fruits and vegetebles are good for you')
+
+// const checkForSpam = function (message) {
+//     if (checkForSpam.includes(Spam, sale)) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+// message("its the best sale for you")
+// console.log(message("its the best sale for you"));
