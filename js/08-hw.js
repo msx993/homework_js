06ed388f21b15c86610dfd7a42120f22864026b0
msx993 <mivanovych2012@gmail.com>
2026-04-26 12:02:26 +0300

    я

--- js/08-hw.js
+++ js/08-hw.js
@@ -3,14 +3,14 @@
 // console.log(string);
 
 
-const cards = [
-"card-1",
-"card-2",
-"card-3",
-"card-4",
-"card-5",
-];
+// const cards = [
+// "card-1",
+// "card-2",
+// "card-3",
+// "card-4",
+// "card-5",
+// ];
 
-const cardToRemove = cards.splice(2, 1)
-const cardToInsert = cards.splice(5, 0, 'card-6')
-console.log(cardToRemove);
+// const cardToRemove = cards.splice(2, 1)
+// const cardToInsert = cards.splice(5, 0, 'card-6')
+// console.log(cards);
---
+++ js/10-hw.js
@@ -0,0 +1,36 @@
+const calculate =(a, b, operation) => {
+    const result = a + b
+    return result
+}
+const calculate =(a, b, operation) => {
+     const result = a - b
+    return result
+}
+const calculate =(a, b, operation) => {
+ const result = a * b
+    return result
+}
+const calculate =(a, b, operation) => {
+ const result = a / b
+    return result
+}
+
+const calculate =(a, c, operation) => {
+ const result = a / c
+    return result
+}
+a(10)
+b(5)
+c(0)
+console.log(
+a(10);
+b(5);
+c(0);
+);
+
+
+//     console.log(calculate(10, 5, add));      // Повинно показати 15
+// console.log(calculate(10, 5, subtract)); // Повинно показати 5
+// console.log(calculate(10, 5, multiply)); // Повинно показати 50
+// console.log(calculate(10, 5, divide));   // Повинно показати 2
+// console.log(calculate(10, 0, divide));   // Повинно показати помилку
